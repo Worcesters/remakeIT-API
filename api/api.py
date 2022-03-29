fc8375e5ae2acd0a70159adad4d3cb56e83af15c
TheoBIET <dev.theobiet@gmail.com>
2022-03-29 15:07:53 +0200

    👂 | Update Flask config to listen on 3000 port

--- api/api.py
+++ api/api.py
@@ -1,12 +1,15 @@
 from os import getenv
 import logging
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 from dotenv import load_dotenv
 
 
 load_dotenv()
-SECRET_KEY = getenv('SECRET_KEY')
+SECRET_KEY=getenv('SECRET_KEY')
+PORT=getenv('PORT')
+HOST='0.0.0.0'
+BASE_URL='/api/v1'
 
 def create_app():
     """
@@ -17,20 +20,41 @@ def create_app():
         level=logging.DEBUG,
         format="%(asctime)s %(name)-12s %(levelname)-8s %(message)s",
         datefmt="%m-%d %H:%M",
-        handlers=[logging.FileHandler("we_api.log"), logging.StreamHandler()]
+        handlers=[logging.FileHandler("api/remakeIT_api.log"), logging.StreamHandler()]
     )
 
     app = Flask(__name__)
     CORS(app)
     app.config['SECRET_KEY'] = SECRET_KEY
 
-    @app.route('/', methods=['GET', 'POST'])
-    def index():
-        return 'Hello World'
+    @app.route(f'{BASE_URL}/', methods=['GET', 'POST'])
+    def welcome():
+        return jsonify(
+            message="Welcome to the Remake IT API!",
+            version="0.1.0",
+            authors=[
+                {
+                    'name': 'Théo BIET',
+                    'github': 'https://github.com/TheoBIET',
+                },
+                {
+                    'name': 'Éric Clouzet',
+                    'github': 'https://github.com/EricClouzet',
+                },
+                {
+                    'name': 'Mickael Milliat',
+                    'github': 'https://github.com/mm-devpro',
+                },
+                {
+                    'name': 'Jérémy',
+                    'github': 'https://github.com/Worcesters',
+                }
+            ]
+        )
 
     return app
 
 
 if __name__ == '__main__':
     app = create_app()
-    app.run(debug=True)
+    app.run(host=HOST, port=PORT, debug=True)
